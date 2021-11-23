3e6e7db8ca417d8c760c3134f7c7f7d58f429b6b
Nikita Malviya <93997551+nikitamalviyafgli@users.noreply.github.com>
2021-11-23 15:57:30 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,4 +3,5 @@ from flask import Flask, render_template, request, jsonify
 
 @app.route('/result', methods=['POST', 'GET'])
 def main():
-  return jsonify({"result":"success"})
+  if request.method == 'POST':
+    return jsonify({"result":"success"})
