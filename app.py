35f19fa2f94a8c6de3a4a16c08d2e2b5e0777c97
Nikita Malviya <93997551+nikitamalviyafgli@users.noreply.github.com>
2021-11-24 10:48:06 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 import numpy as np, pandas as pd, import os, requests
 from flask import Flask, render_template, request, jsonify
 
+
+app = Flask(__name__)
+ 
 @app.route('/result', methods=['POST', 'GET'])
 def main():
   if request.method == 'POST':
