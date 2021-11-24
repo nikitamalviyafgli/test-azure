50b80046a656f51964a9e206a9af25a8bbe55bf9
Nikita Malviya <93997551+nikitamalviyafgli@users.noreply.github.com>
2021-11-24 10:57:31 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,9 +2,14 @@ import numpy as np, pandas as pd, import os, requests
 from flask import Flask, render_template, request, jsonify
 
 
+
 app = Flask(__name__)
  
-@app.route('/result', methods=['POST', 'GET'])
+@app.route('/result', methods=['POST'])
 def main():
   if request.method == 'POST':
     return jsonify({"result":"success"})
+   
+   
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', debug=True)
