8fac3a3ddcb44a87b384aa1089f7672d9a90ad8e
Nikita Malviya <93997551+nikitamalviyafgli@users.noreply.github.com>
2021-11-24 11:01:59 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,8 +8,12 @@ app = Flask(__name__)
 @app.route('/result', methods=['POST'])
 def main():
   if request.method == 'POST':
-    return jsonify({"result":"success"})
-   
+   try:
+    if 'file' not in request.files:
+     return jsonify({"predict":"", "errorcode" : "1", 'errormessage':"File not selected!"})
+    else:
+     uploaded_file = request.files['file']
+     return jsonify({"result":"success"})
    
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
