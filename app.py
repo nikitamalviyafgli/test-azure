5fe1a1c9bc23ca52343fbb51cb0eb69f726a6417
Nikita Malviya <93997551+nikitamalviyafgli@users.noreply.github.com>
2021-11-24 15:38:01 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,19 +1,19 @@
-import numpy as np, pandas as pd, import os, requests
 from flask import Flask, render_template, request, jsonify
-
-
 
 app = Flask(__name__)
  
-@app.route('/result', methods=['POST'])
+@app.route('/index', methods=['POST'])
 def main():
-  if request.method == 'POST':
-   try:
-    if 'file' not in request.files:
-     return jsonify({"predict":"", "errorcode" : "1", 'errormessage':"File not selected!"})
-    else:
-     uploaded_file = request.files['file']
-     return jsonify({"result":"success"})
+    rtext = "dummy"
+    if request.method == 'POST':
+        try:
+            response = request.get_json(force=True)
+            print("\n\nresponse : ", response)
+            rtext = response['test']
+        except Exception as E:
+            print(E)
+    return jsonify({"result": rtext})
    
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=True)
+    app.run(host='0.0.0.0',debug=True)
+    #app.run(host='0.0.0.0', port=8000,debug=True)
