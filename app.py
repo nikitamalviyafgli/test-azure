71db8450c1edbee1bb8706e9067dd8d5f3e907ee
Nikita Malviya <93997551+nikitamalviyafgli@users.noreply.github.com>
2021-12-03 13:25:01 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,19 +1,31 @@
-from flask import Flask, render_template, request, jsonify
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Dec  1 20:18:16 2021
+
+@author: 1134677
+"""
+
+from flask import Flask, jsonify, request
+import sys
 
 app = Flask(__name__)
- 
-@app.route('/index', methods=['POST'])
-def main():
-    rtext = "dummy"
+
+@app.route("/index", methods=["POST"])
+def index():
     if request.method == 'POST':
         try:
             response = request.get_json(force=True)
-            print("\n\nresponse : ", response)
-            rtext = response['test']
-        except Exception as E:
-            print(E)
-    return jsonify({"result": rtext})
-   
-if __name__ == '__main__':
-    app.run(host='0.0.0.0',debug=True)
-    #app.run(host='0.0.0.0', port=8000,debug=True)
+            print(f"\nresponse : {response}")
+            name = response["name"]
+            print(name)
+            print(jsonify({'result':name, 'errorcode': "1",  'errormessage':""}))
+            return jsonify({'result':name, 'errorcode': "1",  'errormessage':""})
+        except Exception as e:
+               print(f"Exception Raised : {e}, errorOnLine: {sys.exc_info()[-1].tb_lineno}")
+               result=""
+               return jsonify({'result':result, 'errorcode': "1",  'errormessage':"Error!"})
+           
+    #return "Hello user!"
+
+if __name__ == "__main__":
+    app.run(host=0.0.0.0, debug=True)
